dd756161b87869b741c426d2582f8b652614e454
C0MPU73R <55300593+C0MPU73R@users.noreply.github.com>
2021-05-14 17:26:48 -0400

    main: Main updated

---
+++ libtree/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "general.h"
+#include "avl.h"
+#include "binary.h"
+#include "binarySearch.h"
+#include "nary.h"
+#include "redBlack.h"
+#include "splay.h"
+int main()
+{
+	return 0;
+}
